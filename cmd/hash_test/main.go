79a49abe70a89497b2a0f1e2d6c22e37268a36e0
jesus m. rodriguez <jesusr@redhat.com>
2022-08-10 23:57:48 -0400

    Print out the go version, OS and arch

--- cmd/hash_test/main.go
+++ cmd/hash_test/main.go
@@ -4,10 +4,11 @@ import (
 	"fmt"
 	"hash"
 	"hash/fnv"
+	"runtime"
 
-	"k8s.io/apimachinery/pkg/util/rand"
 	"github.com/davecgh/go-spew/spew"
 	rukpakv1alpha1 "github.com/operator-framework/rukpak/api/v1alpha1"
+	"k8s.io/apimachinery/pkg/util/rand"
 	// plain "github.com/operator-framework/rukpak/internal/provisioner/plain/types"
 )
 
@@ -22,42 +23,38 @@ func DeepHashObject(hasher hash.Hash, objectToWrite interface{}) {
 	printer.Fprintf(hasher, "%#v", objectToWrite)
 }
 
-func GenerateTemplateHash(template  interface{}) string{
+func GenerateTemplateHash(template interface{}) string {
 	hasher := fnv.New32a()
 	DeepHashObject(hasher, template)
 	return rand.SafeEncodeString(fmt.Sprint(hasher.Sum32()))
 }
 
-func main()  {
+func main() {
+	fmt.Printf("go version: %q, GOOS: %q, GOARCH: %q\n", runtime.Version(), runtime.GOOS, runtime.GOARCH)
 	test_template := rukpakv1alpha1.BundleTemplate{
-						Spec: rukpakv1alpha1.BundleSpec{
-							ProvisionerClassName:"core.rukpak.io/plain",
-							Source: rukpakv1alpha1.BundleSource{
-								Type: rukpakv1alpha1.SourceTypeImage,
-								Image: &rukpakv1alpha1.ImageSource{
-									Ref: "testdata/bundles/plain-v0:valid",
-								},
-							},
-						},
-					}
+		Spec: rukpakv1alpha1.BundleSpec{
+			ProvisionerClassName: "core.rukpak.io/plain",
+			Source: rukpakv1alpha1.BundleSource{
+				Type: rukpakv1alpha1.SourceTypeImage,
+				Image: &rukpakv1alpha1.ImageSource{
+					Ref: "testdata/bundles/plain-v0:valid",
+				},
+			},
+		},
+	}
 	test_pointer := &rukpakv1alpha1.BundleTemplate{
-						Spec: rukpakv1alpha1.BundleSpec{
-							ProvisionerClassName:"core.rukpak.io/plain",
-							Source: rukpakv1alpha1.BundleSource{
-								Type: rukpakv1alpha1.SourceTypeImage,
-								Image: &rukpakv1alpha1.ImageSource{
-									Ref: "testdata/bundles/plain-v0:valid",
-								},
-							},
-						},
-					}
+		Spec: rukpakv1alpha1.BundleSpec{
+			ProvisionerClassName: "core.rukpak.io/plain",
+			Source: rukpakv1alpha1.BundleSource{
+				Type: rukpakv1alpha1.SourceTypeImage,
+				Image: &rukpakv1alpha1.ImageSource{
+					Ref: "testdata/bundles/plain-v0:valid",
+				},
+			},
+		},
+	}
 	hashed_template := GenerateTemplateHash(test_template)
 	hashed_pointer := GenerateTemplateHash(test_pointer)
 	fmt.Printf("hash output (template): %+v\n", hashed_template)
 	fmt.Printf("hash output (pointer): %+v\n", hashed_pointer)
 }
-
-
-
-
-
